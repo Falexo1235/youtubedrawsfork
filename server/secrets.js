89b74458a908dc5f35818cb7fa66acd37a09435a
Falexo1235 <165546517+Falexo1235@users.noreply.github.com>
2025-03-30 00:52:55 +0700

    Add files via upload

---
+++ server/secrets.js
@@ -0,0 +1,9 @@
+
+const dbUri = "mongodb+srv://dbuser:<db_password>@cluster0.aybaufr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const ytClientSecret = "null";
+
+const titleVideoId = "null";
+const thumbnailVideoId = "yourOtherVideoId";
+
+export default {dbUri, ytClientSecret, titleVideoId, thumbnailVideoId};
